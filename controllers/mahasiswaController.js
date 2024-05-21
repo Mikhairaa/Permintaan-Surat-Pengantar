f810f33fc296a9a7278bd9e26674e306577f4210
Rahmatulfadilla <rahmatulfadilla66@gmail.com>
2024-05-22 00:44:08 +0700

    Profil

---
+++ controllers/mahasiswaController.js
@@ -0,0 +1,28 @@
+const User = require('../models/user');
+
+exports.dashboard = (req, res) => {
+  res.render('mahasiswa/mahasiswaDashboard');
+};
+
+exports.lihatProfil = async (req, res) => {
+  try {
+  
+    // console.log(req.userId);
+    const id = req.user.id
+    console.log("pC", id);
+    const lihatProfil = await User.findByPk(id);
+    console.log (lihatProfil)
+    const userId = lihatProfil.id;
+    const userRole = lihatProfil.role;
+    const userEmail = lihatProfil.email;
+    const userNama = lihatProfil.nama;
+    const userNo_Id = lihatProfil.no_id;
+    const userAlamat = lihatProfil.alamat;
+    res.render('mahasiswa/profile', {userId, userRole, userEmail, userNama, userNo_Id, userAlamat});
+    
+  } catch (error) {
+    console.error("Error during login: ", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+  
+}
